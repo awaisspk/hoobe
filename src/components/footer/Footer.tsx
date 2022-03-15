bdafdc4aaae8bdab0ea9a0f91f07ccc21d75604d
awaisspk <iamawaisiqball@gmail.com>
2022-03-16 00:25:58 +0500

    change footer logo'

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <div className="">
       <footer>
-        <div className="relative h-20 w-40 mx-auto mt-10 mb-5">
+        <div className="relative h-20 w-80 mx-auto mt-10 mb-5">
           <Image src="/assets/tappie.jpeg" layout="fill" alt="" />
         </div>
       </footer>
